fd42d6c259bd5aef259bb4766d0fdbb41e39320a
N.Dorves <dorves@gmx.net>
2024-11-25 22:48:22 +0100

    1.added role.choices to user.profile, add route.profile, remove profile. add db.log

--- booking_app/listings/permissions.py
+++ booking_app/listings/permissions.py
@@ -6,3 +6,7 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
         return obj.owner == request.user
+
+
+
+
